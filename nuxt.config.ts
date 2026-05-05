8bfea8eef2baba386c6a5b2e79283e0db7ef4bf0
Wing <114337707+wing-0902@users.noreply.github.com>
2026-05-05 09:23:57 +0000

    UnoCSS Reset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  css: [
+    '@unocss/reset/sanitize/sanitize.css',
+    '@unocss/reset/sanitize/assets.css'
+  ],
   devtools: { enabled: true },
   modules: ['@unocss/nuxt']
 });
