ab51422c4277a6439b5af9b2623c08a340eff7c4
Wing <114337707+wing-0902@users.noreply.github.com>
2026-05-06 01:01:30 +0000

    [skip ci] search ui

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import '@fontsource/zen-kaku-gothic-new';
+import '@fontsource-variable/fira-code/wght.css';
+import '@fontsource-variable/m-plus-1-code/wght.css';
 </script>
 
 <template>
--- app/components/SearchEntry.vue
+++ app/components/SearchEntry.vue
@@ -1,6 +1,20 @@
+<script setup lang="ts">
+const query = ref('');
+</script>
+
 <template>
   <div h-full w-full p-3>
-    <section h-full w-full class="root"></section>
+    <section h-full w-full p-3 class="root">
+      <div h-10 class="searchSlot">
+        <input h-full w-full type="text" v-model="query" />
+        <button
+          h-full
+          w-7
+          i-material-symbols-light:history-2-rounded
+          class="historyButton"
+        ></button>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -8,5 +22,20 @@
 .root {
   backdrop-filter: brightness(1.2);
   border-radius: 16px;
+  .searchSlot {
+    display: flex;
+    input {
+      background-color: var(--codeBack);
+      font-family: 'Fira Code Variable', 'M PLUS 1 Code Variable', monospace;
+      color: white;
+      font-size: 18px;
+      border: none;
+      border-radius: 20px;
+      &:focus {
+        outline: none;
+        color: var(--themeColor);
+      }
+    }
+  }
 }
 </style>
